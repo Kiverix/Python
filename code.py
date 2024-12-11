a944f345675062cd16abfcb6bbb67b51682d9bc5
kiverix <137424284+Kiverix@users.noreply.github.com>
2024-12-11 09:36:55 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,16 @@
+#Making a Class
 class Etudiant:
-    def __init__(self):
-        print("Saisir votre nom:")
-        self.nom = input()
-        print("Saisir votre prenom")
-        self.prenom = input()
-        
+#    def __init__(self):
+#        print("Saisir votre nom:")
+#        self.nom = input()
+#        print("Saisir votre prenom")
+#        self.prenom = input()
+
+    def __init__(self,nm:" ",pr:" "):
+        self.nom = nm
+        self.prenom = pr
+
 bob = Etudiant()
 bob.nom = "Michel"
-print(bob.nom)
+brayan = Etudiant('Dupont',"Paul")
+print(brayan.__dict__)
