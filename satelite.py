07bb7040324116fe35eb4ed182e9c4b337a31c56
kiverix <137424284+Kiverix@users.noreply.github.com>
2024-12-11 10:15:33 +0100

    Update satelite.py

--- satelite.py
+++ satelite.py
@@ -7,8 +7,28 @@
 #du satellite. LA vitesse maximale du satellite est définit comme étant égale au triple de sa masse. 
 
 class satellite:
-    def __init__(sat,mss=100,spd=0):
-        print("Masse du Satellite:")
-        sat.mss = int(input())
-        print("Vitesse du satellite:")
-        sat.spd = int(input())
+    def __init__(sat,mss=100,vel=0):
+        if mss=="" and vel=="":
+            print("Masse du Satellite:")
+            sat.mss = int(input())
+            print("Vitesse du satellite:")
+            sat.vel = int(input())
+        else:
+            sat.mss = mss
+            sat.vel = vel
+    def DeltaV():
+        print("Changer la vitesse:")
+        sat.vel = int(input())
+        
+
+sputnik = satellite(785, 46)
+hubble = satellite(953, 754)
+luna1 = satellite(547, 98)
+surveyor = satellite()
+
+hubble.DeltaV()
+
+print(sputnik.__dict__)
+print(hubble.__dict__)
+print(luna1.__dict__)
+print(surveyor.__dict__)
