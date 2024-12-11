ac05cd5b7294c2498b854708a5815ade8e3d9f7b
kiverix <137424284+Kiverix@users.noreply.github.com>
2024-12-11 09:48:10 +0100

    Create satelite.py

---
+++ satelite.py
@@ -0,0 +1,7 @@
+#Écrire un programme Python qui permet de définir une classe Satellite qui permette d’instancier
+#des objets simulant des satellites artificiels lancés dans l’espace, autour de la terre. Le
+#constructeur de cette classe initialisera les attributs d’instance suivants, avec les valeurs par
+#défaut indiquées : masse = 100, vitesse = 0.
+#Créez plusieurs satellites avec des valeurs différentes.
+#Ajoutez deux méthodes accélérer/ralentir permettant de modifier automatiquement la vitesse
+#du satellite. LA vitesse maximale du satellite est définit comme étant égale au triple de sa masse. 
