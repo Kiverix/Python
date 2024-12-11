b25b3bbdcdfbd0c98fe697a750670d07b97d3054
kiverix <137424284+Kiverix@users.noreply.github.com>
2024-12-11 09:43:23 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,26 @@
 #Making a Class
 class Etudiant:
-#    def __init__(self):
-#        print("Saisir votre nom:")
-#        self.nom = input()
-#        print("Saisir votre prenom")
-#        self.prenom = input()
+    def __init__(self):
+        print("Saisir votre nom:")
+        self.nom = input()
+        print("Saisir votre prenom")
+        self.prenom = input()
 
-    def __init__(self,nm:" ",pr:" "):
-        self.nom = nm
-        self.prenom = pr
+#    def __init__(self,nm:" ",pr:" "):
+#        if nm=="" and pr=="":
+#            print("Saisir votre nom")
+#            self.nom = input()
+#            print("Saisire votre prenom")
+#            self.prenom = input()
+#        else:
+#            self.nom = nm
+#            self.prenom = pr
+
+    def bonjour():
+        print("Je suis ",self.nom," ",self.prenom)
 
 bob = Etudiant()
 bob.nom = "Michel"
-brayan = Etudiant('Dupont',"Paul")
-print(brayan.__dict__)
+#brayan = Etudiant('Dupont',"Paul")
+#print(brayan.__dict__)
+bob.bonjour()
